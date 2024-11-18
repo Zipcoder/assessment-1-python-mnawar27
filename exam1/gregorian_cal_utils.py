8049151861d637f844e08ae4722c85fc19b80f9e
Maisha Nawar <mnawar@Zipcoders-MacBook-Pro.local>
2024-11-18 10:59:59 -0500

    Loopty_loop done

--- exam1/gregorian_cal_utils.py
+++ exam1/gregorian_cal_utils.py
@@ -5,4 +5,6 @@ def is_leap_year(year: int) -> bool:
     :param year: an integer indicating a year.
     :return: A boolean indicating whether or not the year parameter is a leap year.
     """
+
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
     
--- exam1/loopty_loop.py
+++ exam1/loopty_loop.py
@@ -10,7 +10,11 @@ def generate_list(start: int, stop: int, step: int = 1) -> List[int]:
     :param step: How many digits apart each number is from the others around it.
     :return: A list of integers.
     """
-    pass  # implement me
+    a = []
+    for i in range(start, stop, step):
+        a.append(i)
+    return a
+    
 
 
 def generate_list_with_strategy(start: int, stop: int, step: int, strategy: Callable) -> List[int]:
@@ -23,4 +27,8 @@ def generate_list_with_strategy(start: int, stop: int, step: int, strategy: Call
     :param strategy: A function to manipulate each digit .
     :return: A list of integers.
     """
-    pass  # implement me
+    a = []
+    for i in range(start, stop, step):
+        a.append(strategy(i))
+    return a
+    
